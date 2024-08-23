c79c09cf67d76344fc6c086de9dfc799ee04d90d
brownieboi3128 <cmbrown27@catamount.wcu.edu>
2024-08-23 10:57:10 -0400

    ex3

---
+++ Exercise3/Ex3_StringsAndSlices.py
@@ -0,0 +1,13 @@
+# 2. Get values for varibles
+word1 = "frankiln"
+word2 = 'roosevelt'
+
+# 3. Do Caluclations
+half1 = len(word1) // 2
+beginning1 = word1[:half1]
+
+quarter2 = len(word2) // 4
+beginning2 = word2[:quarter2]
+
+# 4. Display results
+print(beginning1, beginning2)
