88c071960ea6ce22baa2d58f78e95e30b59da677
brownieboi3128 <cmbrown27@catamount.wcu.edu>
2024-08-23 10:30:19 -0400

    ex2

---
+++ Exercise1/Ex2_FavMovies.py
@@ -0,0 +1,6 @@
+# 2. Get values for Variable
+movie1 = "how to train your dragon"
+movie2 = "spider-man: into the spiderverse"
+
+# 4. Display results
+print("My favorite movies:\n\t" + movie1.title() + "\n\t" + movie2.title())
