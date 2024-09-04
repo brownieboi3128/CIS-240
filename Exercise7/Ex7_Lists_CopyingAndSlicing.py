f612c67583770aef01eb412496acab4079f4723d
brownieboi3128 <cmbrown27@catamount.wcu.edu>
2024-09-04 10:29:12 -0400

    ex7

---
+++ Exercise7/Ex7_Lists_CopyingAndSlicing.py
@@ -0,0 +1,14 @@
+mygrades = ['- A','- B','- B+','- A-']
+suzyGreenburgGrades = mygrades[:]
+
+mygrades.append('- A+')
+suzyGreenburgGrades.append('- C-')
+
+print("My grades are:")
+for grade in mygrades:
+    print(grade)
+
+print("\nSuzy Greenburg's grades are:")
+for grade in suzyGreenburgGrades:
+    print(grade)
+
