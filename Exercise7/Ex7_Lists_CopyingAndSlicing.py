6035cedfa1b0da6f75be20e668895833c6eef308
brownieboi3128 <cmbrown27@catamount.wcu.edu>
2024-09-04 10:36:43 -0400

    ex7 with more efficent hypen placement

--- Exercise7/Ex7_Lists_CopyingAndSlicing.py
+++ Exercise7/Ex7_Lists_CopyingAndSlicing.py
@@ -1,14 +1,14 @@
-mygrades = ['- A','- B','- B+','- A-']
+mygrades = ['A','B','B+','A-']
 suzyGreenburgGrades = mygrades[:]
 
-mygrades.append('- A+')
-suzyGreenburgGrades.append('- C-')
+mygrades.append('A+')
+suzyGreenburgGrades.append('C-')
 
 print("My grades are:")
 for grade in mygrades:
-    print(grade)
+    print("-" , grade)
 
 print("\nSuzy Greenburg's grades are:")
 for grade in suzyGreenburgGrades:
-    print(grade)
+    print("-" , grade)
 
