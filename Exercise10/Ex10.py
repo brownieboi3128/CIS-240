c0005dfa339e8cfd5f4ef4af33e076aa4a915217
brownieboi3128 <cmbrown27@catamount.wcu.edu>
2024-09-13 10:56:55 -0400

    added months

--- Exercise10/Ex10.py
+++ Exercise10/Ex10.py
@@ -6,13 +6,13 @@ months = 60
 
 # Divide apr by 100 to make a percent, and 12 to make monthly
 monthlyRate = apr / 100 / 12
-print(f"Monthly Payment\t\tInterest Paid\t\tLoan Balance")
+print(f"Month\tMonthly Payment\t\tInterest Paid\t\tLoan Balance")
 # Loop through each month
 for month in range(months):
     interestPaid = moneyOwed * monthlyRate
     moneyOwed = moneyOwed + interestPaid
     moneyOwed = moneyOwed - payment
-    print(f"${payment:,.2f}\t\t${interestPaid:,.2f}\t\t\t${moneyOwed:,.2f}")
+    print(f"{month}\t${payment:,.2f}\t\t${interestPaid:,.2f}\t\t\t${moneyOwed:,.2f}")
     if (moneyOwed - payment < 0):
         print(f"The last payment is ${moneyOwed:,.2f}")
         print("You paid off the loan in", month + 1, "months")
