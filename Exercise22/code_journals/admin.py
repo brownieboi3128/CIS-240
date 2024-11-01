ed957e6135bb4e07331e940841160276b502b725
brownieboi3128 <cmbrown27@catamount.wcu.edu>
2024-11-01 11:00:27 -0400

    did things

--- Exercise22/code_journals/admin.py
+++ Exercise22/code_journals/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-	
-from code_journals.models import Topic
- 
+
+from code_journals.models import Topic, Entry
+
+
 admin.site.register(Topic)
+admin.site.register(Entry)
